abce3548d57b253520f7bb596bb437ae9e91a32a
irinviis <irina.viisanen@gmail.com>
2023-03-25 15:56:54 +0200

    Added Datepicker & addBusFunction

--- Linja/src/app/app.module.ts
+++ Linja/src/app/app.module.ts
@@ -5,11 +5,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { registerLocaleData } from '@angular/common';
 import localeFi from '@angular/common/locales/fi';
 registerLocaleData(localeFi, 'fi');
+import { defineLocale } from 'ngx-bootstrap/chronos';
+import { fiLocale } from 'ngx-bootstrap/locale';
+defineLocale('fi', fiLocale);
 
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ModalModule } from 'ngx-bootstrap/modal';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { HomeComponent } from './home/home.component';
 import { NavComponent } from './nav/nav.component';
 
@@ -24,7 +29,10 @@ import { NavComponent } from './nav/nav.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    ModalModule.forRoot()
+    FormsModule,
+    ReactiveFormsModule,
+    ModalModule.forRoot(),
+    BsDatepickerModule.forRoot()
   ],
   providers: [
     { provide: LOCALE_ID,
--- Linja/src/app/home/home.component.ts
+++ Linja/src/app/home/home.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { IBus } from '../models/bus.model';
 import { BusService } from '../services/bus.service';
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
+import { BsLocaleService } from 'ngx-bootstrap/datepicker';
 
 @Component({
   selector: 'app-home',
@@ -9,10 +12,19 @@ import { BusService } from '../services/bus.service';
 })
 export class HomeComponent implements OnInit {
   busses: IBus[] = [];
+  newBus: IBus = <IBus>{};
+  modalRef?: BsModalRef;
+  bsConfig!: Partial<BsDatepickerConfig>;
 
   constructor(
-    private busService: BusService
-  ) { }
+    private busService: BusService,
+    private modalService: BsModalService,
+    private localeService: BsLocaleService
+  ) {
+    this.bsConfig = {
+      dateInputFormat: 'DD MMMM YYYY'};
+    this.localeService.use('fi');
+   }
 
   ngOnInit(): void {
     this.getBusses()
@@ -22,6 +34,17 @@ export class HomeComponent implements OnInit {
       this.busService.getBusses().subscribe(busses => {
         this.busses = busses;
       })
+  }
+
+  addBus() {
+    this.busService.addBus(this.newBus).subscribe(() => {
+        this.modalService.hide();
+        this.getBusses();
+    })
+  }
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template);
   }
 
 }
